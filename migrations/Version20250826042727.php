ef6afe2ca92679a0d153dbce130366bf03d8cfdb
rali11 <edu_2015@live.com>
2025-08-31 18:47:53 -0300

    feat(migrations): agregar migraciones para las tablas provincia y document

---
+++ migrations/Version20250826042727.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250826042727 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+$this->addSql('DROP INDEX IF EXISTS document_embedding_hnsw_cosine');
+        $this->addSql('ALTER TABLE document ADD content VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE document DROP content');
+        $this->addSql('CREATE INDEX document_embedding_hnsw_cosine ON document (embedding)');
+    }
+}
